ee5e222b087b44786c343451cf8ae5521ea989fc
bossclement <bossclement@gmail.com>
2023-05-09 20:30:36 +0200

    New commit

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 def fizzbuzz():
-	for i in range(1, 101):
-	    result = str(i)
-	    if i % 3 == 0 and i % 5 == 0:
-		    result = "FizzBuzz"
-	    elif i % 3 == 0:
-		    result = "Fizz"
-	    elif i % 5 == 0:
-		    result = "Buzz"
-	    if i < 99:
-		    result += " "
-	    print(result, end="")
-print("\n", end ="")
+    for i in range(1, 101):
+        result = str(i)
+        if i % 3 == 0 and i % 5 == 0:
+            result = "FizzBuzz"
+        elif i % 3 == 0:
+            result = "Fizz"
+        elif i % 5 == 0:
+            result = "Buzz"
+        if i < 99:
+            result += " "
+            print(result, end="")
+    print("\n", end="")
+fizzbuzz()
