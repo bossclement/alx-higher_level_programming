a819bd3af10711b14ad94a72e8b99c7fdcf5e853
bossclement <bossclement@gmail.com>
2023-05-09 18:19:22 +0200

    New commit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for char in range(ord('a'), ord('z')+1):
-    print("{}".format(chr(char)) if chr(char) not in "qe" else '\0', end="")
+    if chr(char) not in "qe":
+        print("{}".format(chr(char)), end="")
