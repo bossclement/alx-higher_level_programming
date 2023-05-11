3fa664cab66025e0b238796833a7da4832126f70
bossclement <bossclement@gmail.com>
2023-05-11 21:48:45 +0200

    New commit

---
+++ 0x02-python-import_modules/101-easy_print.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    argv = sys.argv
+    argc = len(argv)
+
+    if argc != 4:
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
+        sys.exit(1)
+    argv[1] = int(argv[1])
+    argv[3] = int(argv[3])
+    match argv[2]:
+        case "+":
+            print("{}".format(add(argv[1], argv[3])))
+        case "-":
+            print("{}".format(sub(argv[1], argv[3])))
+        case "*":
+            print("{}".format(mul(argv[1], argv[3])))
+        case "/":
+            print("{}".format(div(argv[1], argv[3])))
+        case _:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
