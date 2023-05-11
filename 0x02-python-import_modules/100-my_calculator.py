cceccb9f40348f85969d34b88e0ae267434faa23
bossclement <bossclement@gmail.com>
2023-05-11 22:09:05 +0200

    New commit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -15,4 +15,7 @@ if __name__ == "__main__":
     if op not in ops:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    print("{} {} {} = {}".format(argv[1], op, argv[3], ops[op](argv[1], argv[3])))
+    print("{} {} {} = {}".format(
+        argv[1], op, argv[3], ops[op](argv[1], argv[3])
+        )
+    )
