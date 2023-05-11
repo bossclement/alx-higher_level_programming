2a818f85fc34ebae1e61bfc459af42fb1511e1c8
bossclement <bossclement@gmail.com>
2023-05-11 22:05:58 +0200

    New commit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,15 +10,9 @@ if __name__ == "__main__":
         sys.exit(1)
     argv[1] = int(argv[1])
     argv[3] = int(argv[3])
-    match argv[2]:
-        case "+":
-            print("{} + {} = {}".format(argv[1], argv[3], add(argv[1], argv[3])))
-        case "-":
-            print("{} - {} = {}".format(argv[1], argv[3], sub(argv[1], argv[3])))
-        case "*":
-            print("{} * {} = {}".format(argv[1], argv[3], mul(argv[1], argv[3])))
-        case "/":
-            print("{} / {} = {}".format(argv[1], argv[3], div(argv[1], argv[3])))
-        case _:
-            print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
+    op = argv[2]
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if op not in ops:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    print("{} {} {} = {}".format(argv[1], op, argv[3], ops[op](argv[1], argv[3])))
