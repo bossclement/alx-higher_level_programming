fb378016c122bcf137f386637ddaab7b88d76e0b
bossclement <bossclement@gmail.com>
2023-05-11 19:43:50 +0200

    New commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+
     argc, argv = len(sys.argv), sys.argv
-    if argc > 1:
-        print("{0} {1}".format(argc - 1, "arguments" if argc > 3 else "argument"))
-        for i in range(1, argc):
-            print("{0}: {1}".format(i, argv[i]))
+    print("{0} {1}".format(argc - 1, "arguments" if argc > 3 else "argument"))
+    for i in range(1, argc):
+        print("{0}: {1}".format(i, argv[i]))
