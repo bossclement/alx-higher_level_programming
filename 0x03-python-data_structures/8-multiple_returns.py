bc08716baaa8a6ca4ea80f6019ce11450fb2a3b5
bossclement <bossclement@gmail.com>
2023-05-15 01:46:26 +0200

    New commit

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     return (len(sentence),
-    (None if not sentence else sentence[0]))
+            (None if not sentence else sentence[0])
+            )
