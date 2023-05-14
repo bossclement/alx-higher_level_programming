039f45b164005a31128f76118d6f015a7ca604b5
bossclement <bossclement@gmail.com>
2023-05-15 01:25:08 +0200

    New commit

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2 or len(tuple_b) < 2:
+    if len(tuple_a) < 2 and len(tuple_b) < 2:
+        print(tuple_a, tuple_b, len(tuple_a), len(tuple_b))
         return (0, 0)
-    return ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
+    return (
+        (
+            (tuple_a[0] if len(tuple_a) >= 1 else 0) +
+            (tuple_b[0] if len(tuple_b) >= 1 else 0)),
+        ((tuple_a[1] if len(tuple_a) >= 2 else 0) +
+         (tuple_b[1] if len(tuple_b) >= 2 else 0))
+    )
