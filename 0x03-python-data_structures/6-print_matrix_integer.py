ec73fd26ff076c36b64ee2e6b68b0b781998ccd0
bossclement <bossclement@gmail.com>
2023-05-15 01:00:28 +0200

    New commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for my_list in matrix:
         for index, element in enumerate(my_list):
-            print("{:d}".format(element), end=" " if index < (len(my_list) - 1) else "")
+            print(
+                    "{:d}".format(element),
+                    end=" " if index < (len(my_list) - 1) else ""
+                    )
         print("")
