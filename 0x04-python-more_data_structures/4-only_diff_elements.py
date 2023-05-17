12b9ed078a39ef9b8bf2cecd5bc0ae159b72d75c
bossclement <bossclement@gmail.com>
2023-05-17 21:02:32 +0200

    New commit

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    return set_1.difference(set_2)
+    diff1 = set_1 - set_2
+    diff2 = set_2 - set_1
+    unique = diff1.union(diff2)
+    return unique
