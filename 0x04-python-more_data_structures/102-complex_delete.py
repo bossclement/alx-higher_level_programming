f65787749220a71dc04011f3992b07e3fa2d24f0
bossclement <bossclement@gmail.com>
2023-05-17 23:43:39 +0200

    New commit

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     for key in list(a_dictionary.keys()):
-        key_value = a_dictionary[key]
-        if key_value == value:
-            del a_dictionary[key_value]
+        if a_dictionary[key] == value:
+            del a_dictionary[key]
     return a_dictionary
