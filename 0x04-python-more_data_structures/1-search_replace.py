da5ff00e5590fd2df9228ccbcf9b3786e1197b61
bossclement <bossclement@gmail.com>
2023-05-17 20:26:32 +0200

    New commit

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return [
-        int(i) for i in (",".join([str(num) for num in my_list]).
-                         replace("{},".format(search), "{},".
-                         format(replace)).split(","))
-    ]
+    new_list = []
+    for i in my_list:
+        if i == search:
+            new_list.append(replace)
+        else:
+            new_list.append(i)
