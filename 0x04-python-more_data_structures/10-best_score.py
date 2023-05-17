0c91b0fc4b13c2dda44cc9ae9ad8cbc18e89e64b
bossclement <bossclement@gmail.com>
2023-05-17 21:44:30 +0200

    New commit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     biggest = {None: 0}
-    for key, value in a_dictionary.items():
-        if biggest.values()[0] < value:
-            biggest = {key: value}
-    return biggest.keys()[0]
+    if a_dictionary:
+        for key, value in a_dictionary.items():
+            if list(biggest.values())[0] < value:
+                biggest = {key: value}
+    return list(biggest.keys())[0]
