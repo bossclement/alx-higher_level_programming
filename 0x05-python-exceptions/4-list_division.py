52d8320e33944e712213ef12e5e16fac2da1912d
bossclement <bossclement@gmail.com>
2023-05-29 20:38:38 +0200

    New commit

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    result = 0
 
     for i in range(len(my_list_1)):
-        result = 0
         try:
             result = my_list_1[i] / my_list_2[i]
         except TypeError:
