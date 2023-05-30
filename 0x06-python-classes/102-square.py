3bbf16ec6d9b53d5afb41148a6e4ba5f7b917b28
bossclement <bossclement@gmail.com>
2023-05-31 01:44:41 +0200

    New commit

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Documantation for the module
+"""
+
+
+class Square:
+    """Class docstring"""
+
+    def __init__(self, size=0):
+        """Constructor function"""
+        self.__size = size
+
+    def area(self):
+        """Calculates the area of the square"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Retrives the value of size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the value of size"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
