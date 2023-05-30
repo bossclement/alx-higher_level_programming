f41dff4c800425e16b9add2080d192d9504a3a5f
bossclement <bossclement@gmail.com>
2023-05-30 20:07:57 +0200

    New commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+This is documantion for this module
+"""
+
+
+class Square:
+    """This is my class documantation"""
+    def __init__():
+        """This is a constructor function"""
+        self.__size = 3
