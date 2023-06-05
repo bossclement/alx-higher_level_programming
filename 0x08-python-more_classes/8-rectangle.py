cccf66c0a407c941d0f9ef418742a0ec555ed70b
bossclement <bossclement@gmail.com>
2023-06-05 23:05:28 +0200

    New commit

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -96,7 +96,7 @@ class Rectangle:
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
-    @staticmethod
+    @classmethod
     def bigger_or_equal(rect_1, rect_2):
         """checks the biggest rectangle based on the area
         Returns:
