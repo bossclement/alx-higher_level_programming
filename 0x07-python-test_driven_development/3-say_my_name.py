a59f9d1c4fbc33358c0a72f6e2d792ea1cadb80d
bossclement <bossclement@gmail.com>
2023-06-07 19:38:37 +0200

    New commit

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -8,13 +8,15 @@ respectively, then prints out the full name.
 
 
 def say_my_name(first_name, last_name=""):
-    """Returns:
+    """Print the full name of a person
+    Args:
+        first_name (str): the first name.
+        last_name (str): the last name.
+    Returns:
         None
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-
     print("My name is {} {}".format(first_name, last_name))
