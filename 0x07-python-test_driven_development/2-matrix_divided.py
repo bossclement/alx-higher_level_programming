2a5826283c0972ca1f234fd46ec853ddf04ce47b
bossclement <bossclement@gmail.com>
2023-06-07 19:20:58 +0200

    New commit

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,25 +8,28 @@ having 2 decimal places only.
 
 
 def matrix_divided(matrix, div):
-    """Returns:
+    """Divides matrix with a provided divider
+    Args:
+        matrix (list): the matrix.
+        div (int): the devider.
+    Returns:
         (list): new matrix
     """
 
-    if not all(isinstance(row, list) and
-               all(isinstance(num, (int, float)) for num in row)
-               for row in matrix):
-        raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats")
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all((isinstance(ele, int) or isinstance(ele, float))
+                    for ele in [num for row in matrix for num in row])):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
 
-    if len(set(len(row) for row in matrix)) > 1:
+    if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
-    if not isinstance(div, (int, float)):
+    if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    new_matrix = [[round(num / div, 2) for num in row] for row in matrix]
-
-    return new_matrix
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
