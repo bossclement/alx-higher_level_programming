ca1eb0a3c1c96470585025867a3c223dea082538
bossclement <bossclement@gmail.com>
2023-06-13 01:25:16 +0200

    New commit

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -6,4 +6,10 @@ class BaseGeometry:
     """Docs for my class goes here"""
 
     def area(self):
+        """
+        Calculate the area.
+        Raises an exception indicating that the area() method
+        is not implemented.
+        """
+
         raise Exception("area() is not implemented")
