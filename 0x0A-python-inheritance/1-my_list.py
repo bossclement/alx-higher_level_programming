2c0c686c7945e4ef44eaa5cc5a9782f4308a2f95
bossclement <bossclement@gmail.com>
2023-06-12 21:15:02 +0200

    New commit

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Docs for my module goes here"""
+
+
+class MyList(list):
+    """Docs for my class goes here"""
+
+    def print_sorted(self):
+        i = [
+            int(x) for x in map(lambda x: x.replace("[", "").
+                                replace("]", "").strip(),
+                                self.__str__().split(',')) if x.isnumeric()
+        ]
+        print(i)
