6fcf966ebc9588f213ce5eec404a7df2522c229b
bossclement <bossclement@gmail.com>
2023-06-12 22:56:01 +0200

    New commit

---
+++ 0x0A-python-inheritance/    3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Docs for my module goes here"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Returns true if an object is an
+    instance of a specified class
+    Args:
+        obj: the object.
+        a_class: the class.
+    Returns:
+        bool: True if yes, False if not.
+    """
+    return type(obj) == a_class or issubclass(a_class)
