c28c3cc6842e35e0c782bba2b56799b5ceaad315
bossclement <bossclement@gmail.com>
2023-06-12 21:47:15 +0200

    New commit

--- 0x0A-python-inheritance/test.py
+++ 0x0A-python-inheritance/test.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 MyList = __import__('1-my_list').MyList
 
-my_list = MyList()
+my_list = MyList(["1"])
+my_list.print_sorted()
 my_list.append(1)
 my_list.append(4)
 my_list.append(2)
 my_list.append(3)
-my_list.append(None)
+my_list.append(float("inf"))
 print(my_list)
 my_list.print_sorted()
 print(my_list)
