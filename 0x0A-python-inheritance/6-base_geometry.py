5ca2493843328840f097a3cfcff4ee26b2090a84
bossclement <bossclement@gmail.com>
2023-06-12 23:16:54 +0200

    New commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Docs for my module goes here"""
+
+
+class BaseGeometry:
+    """Docs for my class goes here"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
