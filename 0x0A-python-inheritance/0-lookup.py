34f68000b21fc11e2ae8555b8ca6ba23f199669b
bossclement <bossclement@gmail.com>
2023-06-12 20:50:12 +0200

    New commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+"""Docs for my module goes here"""
+
+
+def lookup(obj):
+    """Returns list of object attributes
+    Args:
+        obj: the object to get attributes from.
+    Returns:
+        list: attributes of the obj.
+    """
+    return (dir)
