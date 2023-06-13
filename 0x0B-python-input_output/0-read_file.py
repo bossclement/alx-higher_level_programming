101a65b90ddbcc8858af97fe93722bc5551d36de
bossclement <bossclement@gmail.com>
2023-06-13 18:21:18 +0200

    New commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module that reads text file and prints it to the stdout."""
+
+
+def read_file(filename=""):
+    """Reads a text file and prints it to the stdout.
+    :param filename: file name of the file to read.
+    """
+    with open(filename) as f:
+        print(file.read())
+    f.close()
