c4704711dc8490b995370cf1f6b726548ad71df9
bossclement <bossclement@gmail.com>
2023-06-13 20:16:36 +0200

    New commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,11 +3,23 @@
 
 
 import sys
+import json
 
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-filename = "add_item.json"
 
-save_to_json_file(sys.argv[1:], filename)
-print(load_from_json_file(filename))
+
+filename = "add_item.json"
+with open(filename, encoding="utf-8", mode="a+") as f:
+    f.close()
+
+if len(sys.argv) < 2:
+    sys.exit(0)
+
+with open(filename) as f:
+    old_data = json.load(f)
+
+old_data = [*old_data, *sys.argv[1:]]
+save_to_json_file(old_data, filename)
+print(json.dumps(load_from_json_file(filename)))
