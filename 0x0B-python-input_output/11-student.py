d56a3d9864ecdee16a14943c93bc77c936e14f60
bossclement <bossclement@gmail.com>
2023-06-13 21:48:20 +0200

    New commit

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module that represents a student."""
+
+
+class Student:
+    """Blue print of a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves attributes of student."""
+        attributes = self.__dict__
+        result = {}
+        if type(attrs) == list:
+            for attr in attrs:
+                if attr in attributes:
+                    result.update({attr: attributes[attr]})
+                elif type(attr) != str:
+                    return attributes
+            return result
+        return attributes
+
+    def reload_from_json(self, json):
+        """Updates all attributes of a class."""
+        self.__dict__.update(json)
