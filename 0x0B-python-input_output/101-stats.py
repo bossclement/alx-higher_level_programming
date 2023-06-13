d96f4afd7efe16b37cd28b7060a8577cbd0d692e
bossclement <bossclement@gmail.com>
2023-06-13 22:38:41 +0200

    New commit

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -10,7 +10,7 @@ def print_statistics(file_sizes, status_codes):
     """Prints the statistics on stdout"""
     total_size = sum(file_sizes)
     print(f"Total file size: {total_size}")
-
+    
     sorted_status_codes = sorted(status_codes.keys())
     for code in sorted_status_codes:
         count = status_codes[code]
@@ -22,24 +22,25 @@ def process_input():
     file_sizes = []
     status_codes = defaultdict(int)
     line_count = 0
-
+    
     try:
         for line in sys.stdin:
             line = line.strip()
             if line:
+                # Parse the line and extract relevant information
                 parts = line.split()
-                if len(parts) >= 7:
-                    file_size = int(parts[-1])
-                    status_code = parts[-2]
-                    file_sizes.append(file_size)
-                    status_codes[status_code] += 1
-
+                file_size = int(parts[-1])
+                status_code = parts[-2]
+                file_sizes.append(file_size)
+                status_codes[status_code] += 1
+                
                 line_count += 1
-
+                
+                # Print statistics every 10 lines
                 if line_count % 10 == 0:
                     print_statistics(file_sizes, status_codes)
                     print()
-
+            
     except KeyboardInterrupt:
         print_statistics(file_sizes, status_codes)
 
