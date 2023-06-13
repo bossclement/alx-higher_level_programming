06267d328b61e3986c375630259356ccb12ad2be
bossclement <bossclement@gmail.com>
2023-06-13 18:22:30 +0200

    New commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,6 @@ def read_file(filename=""):
     """Reads a text file and prints it to the stdout.
     :param filename: file name of the file to read.
     """
-    with open(filename) as f:
+    with open(filename, encoding="utf-8") as f:
         print(file.read())
     f.close()
