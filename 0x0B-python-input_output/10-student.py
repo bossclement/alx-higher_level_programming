c040016a68443d25d13ced937d0e53a0a0bdff1c
bossclement <bossclement@gmail.com>
2023-06-13 21:31:42 +0200

    New commit

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -15,9 +15,11 @@ class Student:
         """Retrieves attributes of student."""
         attributes = self.__dict__
         result = {}
-        if attrs:
+        if type(attrs) == list:
             for attr in attrs:
                 if attr in attributes:
                     result.update({attr: attributes[attr]})
+                elif type(attr) != str:
+                    return attributes
             return result
         return attributes
