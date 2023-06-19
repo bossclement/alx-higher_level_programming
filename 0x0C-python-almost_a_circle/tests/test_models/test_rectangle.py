147beb09e9a8250ff29fe7b5013ce27049059cff
bossclement <bossclement@gmail.com>
2023-06-19 20:30:36 +0200

    New commit

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,16 +7,6 @@ import unittest
 class TestRectangle(unittest.TestCase):
     """Test my rectangle class."""
 
-    def test_id(self):
-        r1 = Rectangle(10, 2, 1, 1)
-        self.assertEqual(r1.id, 1)
-
-        r2 = Rectangle(2, 10, 1, 1)
-        self.assertEqual(r2.id, 2)
-
-        r3 = Rectangle(10, 2, 1, 1, 12)
-        self.assertEqual(r3.id, 12)
-
     def test_width(self):
         with self.assertRaises(TypeError):
             r1 = Rectangle(None, None)
@@ -26,3 +16,13 @@ class TestRectangle(unittest.TestCase):
 
         r3 = Rectangle(10, 2, 1, 1, 12)
         self.assertEqual(r3.width, 10)
+
+    def test_height(self):
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(None, None)
+
+        r2 = Rectangle(2, 10, 1, 1)
+        self.assertEqual(r2.height, 10)
+
+        r3 = Rectangle(10, 2, 1, 1, 12)
+        self.assertEqual(r3.height, 2)
