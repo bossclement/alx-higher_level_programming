b3fa680088cfd63cd64746019ef42aeca10e7e93
bossclement <bossclement@gmail.com>
2023-06-19 22:38:57 +0200

    New commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Will have the base class for other classes for my project"""
+import json
 
 
 class Base:
@@ -14,3 +15,7 @@ class Base:
             self.id = self.__nb_objects
         else:
             self.id = id
+
+    def to_json_string(list_dictionaries):
+        """returns json string"""
+        return json.dumps(list_dictionaries)
