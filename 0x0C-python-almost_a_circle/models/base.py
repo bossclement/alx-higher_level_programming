fddae66b6a6874fd82425ad7bd2391940339e9ef
bossclement <bossclement@gmail.com>
2023-06-19 22:41:02 +0200

    New commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,6 +18,6 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """returns json string"""
-        if type(list_dictionaries) is not list:
+        if not list_dictionaries:
             return "[]"
         return json.dumps(list_dictionaries)
