e4b71938bca8ddc4ecb3cb3f2530c33b3ee1aa39
bossclement <bossclement@gmail.com>
2023-07-20 20:21:58 +0200

    New commit

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,2 @@
 -- Lists the number of records with the same score
-SELECT score, COUNT(score)'number'
-FROM second_table
-GROUP BY score
-ORDER BY `
-number` DESC;
+SELECT score, COUNT(score)'number' FROM second_table GROUP BY score ORDER BY `number` DESC;
