581ad432fcba0613e0ba97e81efbf2711d5c2e6c
bossclement <bossclement@gmail.com>
2023-07-20 14:16:50 +0200

    New commit

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,11 +1,11 @@
 -- Creates table
-CREATE TABLE second_table (
+CREATE TABLE IF NOT EXISTS second_table (
 	id INT,
 	name VARCHAR(256),
 	score INT
 )
 -- Inserts rows
-INSERT INTO second_table (id, name, score)
+INSERT INTO second_table (`id`, `name`, `score`)
 VALUES
 	(1, "John", 10),
 	(2, "Alex", 3),
