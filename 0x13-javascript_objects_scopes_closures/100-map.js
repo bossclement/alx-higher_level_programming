07b5278e04a6b33e4dee2d24732cbc6cfb4c93a3
bossclement <bossclement@gmail.com>
2023-08-15 21:35:45 +0200

    New commit

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -3,6 +3,6 @@ const list = require('./100-data').list;
 
 const newList = list.map((value, index) => value * index);
 
-console.log('Original list:', list);
-console.log('New list:', newList);
+console.log(list);
+console.log(newList);
 
