4ce471337ee5b17ab8ad9dde2820434c94d5284a
bossclement <bossclement@gmail.com>
2023-08-15 21:09:15 +0200

    New commit

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,12 @@
+#!/usr/bin/nodejs
+
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (let element of list) {
+    if (element === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};
+
