4c7cd9ed92274482ecf701c618913e2d3f125584
bossclement <bossclement@gmail.com>
2023-08-15 19:53:50 +0200

    New commit

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -24,15 +24,5 @@ class Rectangle {
     this.height *= 2;
   }
 }
-const r1 = new Rectangle(2, 3);
-console.log('Normal:');
-r1.print();
 
-console.log('Double:');
-r1.double();
-r1.print();
-
-console.log('Rotate:');
-r1.rotate();
-r1.print();
 module.exports = Rectangle;
