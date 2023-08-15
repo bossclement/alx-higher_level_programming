e7db6599268d22e00d0feaf77afb14246a3aa2a1
bossclement <bossclement@gmail.com>
2023-08-15 19:29:46 +0200

    New commit

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/nodejs
 class Rectangle {
- constructor(w, h) {
-  this.w = w;
-  this.h = h;
- }
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 }
 
 module.exports = Rectangle;
