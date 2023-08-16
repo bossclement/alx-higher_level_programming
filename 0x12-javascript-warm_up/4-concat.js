f385efe759066124bdb4eae477a193dfa46f14a6
bossclement <bossclement@gmail.com>
2023-08-16 22:37:23 +0200

    New commit

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,12 +1,2 @@
 #!/usr/bin/nodejs
-let words;
-if (process.argv[1]) {
-  words = 'undefined' + ' is undefined';
-}
-if (process.argv[2]) {
-  words = process.argv[2] + ' is undefined';
-}
-if (process.argv[3]) {
-  words = process.argv[2] + ' is ' + process.argv[3];
-}
-console.log(words);
+console.log(`${process.argv[2]} is ${process.argv[3]}`);
