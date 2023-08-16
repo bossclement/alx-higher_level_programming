c3a9db603c82db281dc82df22ca558742c30987a
bossclement <bossclement@gmail.com>
2023-08-16 22:50:20 +0200

    New commit

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const arg = process.argv[2];
+
+if (isNaN(arg)) {
+  console.log('Missing number of occurrences');
+} else {
+  const x = parseInt(arg);
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
