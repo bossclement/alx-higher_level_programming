7cb6ca3a382f4305d69e9c64cf338a005d3619ab
bossclement <bossclement@gmail.com>
2023-08-24 16:14:21 +0200

    New commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,12 +8,13 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
-    
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=database)
     cursor = db.cursor()
     cursor.execute("SELECT id, name FROM states ORDER BY states.id ASC")
     rows = cursor.fetchall()
