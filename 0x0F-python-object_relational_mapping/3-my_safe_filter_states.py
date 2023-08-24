901d9adc85c2773efce810146152338756271ccb
bossclement <bossclement@gmail.com>
2023-08-24 17:20:02 +0200

    New commit

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -22,8 +22,9 @@ if __name__ == "__main__":
                          user=username, passwd=password, db=database)
     cursor = db.cursor()
     cursor.execute(
-        "SELECT * FROM states WHERE name LIKE \
-                    BINARY %(name)s ORDER BY states.id ASC", {'name': state_name})
+        """SELECT * FROM states
+        WHERE name LIKE BINARY %(name)s
+        ORDER BY states.id ASC""", {'name': state_name})
     rows = cursor.fetchall()
     for row in rows:
         print(row)
