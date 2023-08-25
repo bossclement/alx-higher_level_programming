f4c4c25e236e3d8d5c378fb4624cf7cb6e61d019
bossclement <bossclement@gmail.com>
2023-08-25 19:47:23 +0200

    New commit

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Adds a state in database
+"""
+
+from model_state import State, Base
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit(1)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = State()
+    state.name = "Louisiana"
+
+    session.add(state)
+    session.commit()
+
+    print(state.id)
