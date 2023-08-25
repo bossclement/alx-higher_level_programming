8b7db3e9c77103fa02b3a6aa5b761a3e80ebcab2
bossclement <bossclement@gmail.com>
2023-08-25 17:06:50 +0200

    New commit

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+fetchs all rows by matching column with a specific value
+and prevents sql injection
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(
+            sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=database)
+    cursor = db.cursor()
+    cursor.execute(
+        """SELECT cities.id, cities.name, states.name
+        FROM states RIGHT JOIN cities
+        ON states.id = cities.state_id
+        ORDER BY cities.id ASC""")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
