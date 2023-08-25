87172eb7ebaceb80b585195542d2671c7bd9dec4
bossclement <bossclement@gmail.com>
2023-08-25 18:54:18 +0200

    New commit

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Lists all states in hbtn_0e_6_usa database
+"""
+
+from model_state import State, Base
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit(1)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    rows = session.query(State).all()
+    for row in rows:
+        print("{}: {}".format(row.id, row.name))
