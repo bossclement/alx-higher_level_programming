6651625d21c6372fb496bc1dfbdc0d185e68b9c0
bossclement <bossclement@gmail.com>
2023-08-25 19:59:53 +0200

    New commit

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Adds a state in database
+"""
+
+from model_state import State, Base
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit(1)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter_by(id=2).first()
+    if state:
+        state.name = "New Mexico"
+    session.commit()
