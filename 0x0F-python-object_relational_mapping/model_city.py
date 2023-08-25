459b473e1eb47858bbaae80bebba5dc42917a600
bossclement <bossclement@gmail.com>
2023-08-25 21:06:46 +0200

    New commit

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -9,6 +9,13 @@ from model_state import Base, State
 
 
 class City(Base):
+    """
+    class that defines schema of cities table.
+    Attributes:
+        id (int): id of the city
+        name (str): city name
+        __tablename__ (str): name of the table in database
+    """
     __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
