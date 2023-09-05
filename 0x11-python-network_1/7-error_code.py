e33c138847d3ed0c25726f4d68f82f091a4ee3f0
bossclement <bossclement@gmail.com>
2023-09-06 01:23:15 +0200

    New commit

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Sends a GET request to a URL, displays
+the response body, and handles HTTP status codes.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+
+    if response.status_code >= 400:
+        print("Error code:", response.status_code)
+    else:
+        print(response.text)
+
