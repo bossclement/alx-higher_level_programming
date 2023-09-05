2f3a2acafa7f97592a3ae41457feea84909bc577
bossclement <bossclement@gmail.com>
2023-09-06 01:34:43 +0200

    New commit

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,25 +5,17 @@ Lists 10 most recent commits of a GitHub repository.
 import sys
 import requests
 
+
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: {} <owner> <repository>".format(sys.argv[0]))
-        sys.exit(1)
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-    owner = sys.argv[1]
-    repository = sys.argv[2]
-
-    github_api_url = "https://api.github.com/repos/{}/{}/commits".format(
-        owner, repository)
-
-    response = requests.get(github_api_url)
-    commit_data = response.json()
-
+    r = requests.get(url)
+    commits = r.json()
     try:
         for i in range(10):
-            commit = commit_data[i]
-            sha = commit.get("sha")
-            author_name = commit.get("commit").get("author").get("name")
-            print("{}: {}".format(sha, author_name))
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
     except IndexError:
         pass
