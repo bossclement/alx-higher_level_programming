5ebf29f48b5ec4fc49cb78858a401c4b24661578
bossclement <bossclement@gmail.com>
2023-09-06 00:48:21 +0200

    New commit

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python3
 """
-Finds peak from numbers stored in a text file
+This script finds a peak in an unsorted list of integers.
 """
 
 def find_peak(list_of_integers):
-    if not list_of_integers:
+    """
+    Find a peak in a list of integers.
+
+    Args:
+        list_of_integers (list): List of integers.
+
+    Returns:
+        int or None: The peak element in the list, or None if the list is empty.
+    """
+    size = len(list_of_integers)
+    mid_e = size
+    mid = size // 2
+
+    if size == 0:
         return None
 
-    left, right = 0, len(list_of_integers) - 1
-
-    while left < right:
-        mid = (left + right) // 2
-        if list_of_integers[mid] > list_of_integers[mid + 1]:
-            right = mid
+    while True:
+        mid_e = mid_e // 2
+        if (mid < size - 1 and
+                list_of_integers[mid] < list_of_integers[mid + 1]):
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid + mid_e // 2
+        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid - mid_e // 2
         else:
-            left = mid + 1
-
-    return list_of_integers[left]
+            return list_of_integers[mid]
