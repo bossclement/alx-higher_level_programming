fcd287bcd498bdc7846c4a6ae64843e4263c7a36
bossclement <bossclement@gmail.com>
2023-09-06 01:07:07 +0200

    New commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Fetches https://alx-intranet.hbtn.io/status using urllib
+"""
+
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+    utf8_content = body.decode('utf-8')
+
+print("Body response:")
+print("\t- type: {}".format(type(body)))
+print("\t- content: {}".format(body))
+print("\t- utf8 content: {}".format(utf8_content))
