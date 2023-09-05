28b8aa49efb8e57cc4a75ab03ffb5165ba389427
bossclement <bossclement@gmail.com>
2023-09-06 00:40:23 +0200

    New commit

---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# ends a DELETE request to the URL passed as the first argument
+curl -sX DELETE "$1"
---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Finds peak from numbers stored in a text file
+"""
+
+def find_peak(list_of_integers):
+    if not list_of_integers:
+        return None
+
+    left, right = 0, len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            right = mid
+        else:
+            left = mid + 1
+
+    return list_of_integers[left]
