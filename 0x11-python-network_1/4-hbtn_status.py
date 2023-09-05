7bc20e849c844da744c3cb8db30f274ad0d6f28f
bossclement <bossclement@gmail.com>
2023-09-06 01:17:48 +0200

    New commit

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Fetches a URL using the requests library
+and displays the response body.
+"""
+
+import requests
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
